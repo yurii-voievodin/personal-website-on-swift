6b01557bb37379cb95ce000e1e6cbaeaacbc0299
Yura Voevodin <voevodin.yura@icloud.com>
2017-06-25 19:57:27 +0300

    Template for posts with HTML content.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     targets: [
         Target(name: "App"),
         Target(name: "Run", dependencies: ["App"]),
-    ],
+        ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
@@ -18,5 +18,5 @@ let package = Package(
         "Database",
         "Public",
         "Resources",
-    ]
+        ]
 )
--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -10,34 +10,7 @@ final class Routes: RouteCollection {
         // Pages
         builder.resource("/", PageController(view))
         
-        // Blog
+        // Posts
         builder.resource("posts", PostController(view))
-        
-        // response to requests to /info domain
-        // with a description of the request
-        builder.get("info") { req in
-            return req.description
-        }
-    }
-    
-    // TODO: Refactor to middleware
-    fileprivate func addRequest() {
-        if let (day, month, year) = Date().calendarComponents {
-            do {
-                if let session = try Session.makeQuery()
-                    .filter("year", year)
-                    .filter("month", month)
-                    .filter("day", day)
-                    .first() {
-                    session.requests += 1
-                    try session.save()
-                } else {
-                    let newSession = Session(day: day, month: month, year: year, requests: 1)
-                    try newSession.save()
-                }
-            } catch  {
-                print(error.localizedDescription)
-            }
-        }
     }
 }
